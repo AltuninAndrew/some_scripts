487a8ca518814a24f4b0e7a80e5438e6c3761363
Andrew Altunin <33705016+AltuninAndrew@users.noreply.github.com>
2019-06-13 18:42:07 +0300

    Update TxtSearcher.cs

--- TxtSearcher.cs
+++ TxtSearcher.cs
@@ -4,44 +4,80 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace TextSearcher.Models
 {
-    class Program
+    public class TxtSearcher
     {
-        static void Main(string[] args)
+        public string RootDirectory { get; set; }
+        public string PatternFileName { get; set; }
+        public string Pattern { get; set; }
+        public string CurProcessingFileName { get; private set; }
+        public int CountProcessingFile { get; private set; }
+
+        private List<string> _foundFileNames = new List<string>();
+        private bool _isSearch;
+        
+        public TxtSearcher(string path,string patternFileName, string pattern)
         {
-            string directoryName = null;
-            string patternFileName = null;
-            List<string> fileNames;
-            string patternText = null;
-            
+            RootDirectory = path;
+            PatternFileName = patternFileName;
+            Pattern = pattern;
+        }
 
-            directoryName = Console.ReadLine();
-            patternFileName = Console.ReadLine();
-            patternText = Console.ReadLine();
+        public void StartSearch()
+        {
+            _isSearch = true;
+            Search();
+        }
 
-            fileNames = Directory.GetFiles(@directoryName, patternFileName,SearchOption.AllDirectories).ToList();
-          
+        public void PauseSearch()
+        {
+            _isSearch = false;
+        }
+
+        public void StopSearch()
+        {
+            _isSearch = false;
+            CountProcessingFile = 0;
+        }
+   
+        private void Search()
+        {
             RegexOptions options = RegexOptions.IgnoreCase;
-            Regex regex = new Regex(patternText, options);
+            Regex regex = new Regex(Pattern, options);
 
-            foreach (var element in fileNames)
+            List<string> files = FileLoader.GetFileNames(RootDirectory, PatternFileName, true);
+
+            for(int i = CountProcessingFile; i<files.Count;i++)
             {
-                //data.Add(File.ReadAllText(element, Encoding.Default));
-
-                using (StreamReader sr = new StreamReader(element, Encoding.Default))
+                if(_isSearch)
                 {
-                    //data.Add(sr.ReadToEnd());
-                    //sr.ReadToEnd();
-                    Console.WriteLine("File name: {0}, Match: {1}",element,regex.IsMatch(sr.ReadToEnd()));
-                }
+                    CountProcessingFile++;
+                    CurProcessingFileName = files[i];
+                    using (StreamReader sr = new StreamReader(files[i],Encoding.Default))
+                    {
+                        if (regex.IsMatch(sr.ReadToEnd()))
+                        {
+                            _foundFileNames.Add(files[i]);
+                        }
+                    }
 
+                }
+                else
+                {
+                    break;
+                }
+               
             }
 
-            
-            Console.ReadKey();
-
         }
+
+        public List<string> GetFoundFiles()
+        {
+            return _foundFileNames;
+        }
+
     }
 }
